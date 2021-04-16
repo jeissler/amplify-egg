04ce19d168b908b0ea3d784624bb4694de102f30
Jeremy Eissler <jeissler0@gmail.com>
2021-04-16 10:53:20 -0500

    add amplify & poc ex files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { Auth } from 'aws-amplify';
+import { useState } from 'react';
+
 function App() {
+  const [user, setUser] = useState({ username: '', password: '', email: '', phone_number: '', authenticationCode: '' });
+  const [step, setStep] = useState(0);
+
+  const onChange = e => {
+    setUser(Object.assign({}, user, { [e.target.name]: e.target.value }));
+  }
+
+  const signUp = async () => {
+    const { username, password, email, phone_number } = user;
+    try {
+      await Auth.signUp({ username, password, attributes: { email, phone_number } })
+      setStep(1);
+      console.log('successful signup!')
+    } catch (err) {
+      console.log('error signing up! ', err)
+    }
+  }
+
+  const confirmSignup = async () => {
+    const { username, authenticationCode } = user;
+    try {
+      await Auth.confirmSignUp(username, authenticationCode)
+      console.log('user signup success')
+    } catch (err) {
+      console.log('error confirming signup! ', err)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        step === 0 && (
+          <div>
+            <input placeholder='user name' onChange={onChange} name='username' style={styles.input} />
+            <input placeholder='password' onChange={onChange} name='password' style={styles.input} />
+            <input placeholder='email' onChange={onChange} name='email' style={styles.input} />
+            <input placeholder='phone number' onChange={onChange} name='phone_number' style={styles.input} />
+            <button onClick={signUp}>Sign Up</button>
+          </div>
+        )
+      }
+      {
+        step === 1 && (
+          <div>
+            <input placeholder='user name' onChange={onChange} name='username' style={styles.input} />
+            <input placeholder='authentication code' onChange={onChange} name='authenticationCode' style={styles.input} />
+            <button onClick={confirmSignup}>Confirm Sign Up</button>
+          </div>
+        )
+      }
     </div>
   );
+}
+
+const styles = {
+  input: {
+    height: 35, margin: 10
+  }
 }
 
 export default App;
